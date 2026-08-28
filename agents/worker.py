b0d84bba9f4a2a5e9db87d2cf28a2a02fb591e0c
wuzhiping <wuzhiping@users.noreply.github.com>
2026-08-28 15:59:42 +0800

    Update worker.py

--- agents/worker.py
+++ agents/worker.py
@@ -9,17 +9,19 @@ from utils.get_config import Config
 
 async def worker():
     # 获取配置
-    config = await Config().fetch()
+    config = await Config(token="123456").fetch()
     # print(config)
     
     _t = config.get("temporal")
-    _addr, _ns, _queue = _t.get("addr"), _t.get("namespace"), _t.get("task_queue")
+    _addr, _ns, _queue ,_rpc_metadata= _t.get("addr"), _t.get("namespace"), _t.get("task_queue"),_t.get("rpc_metadata")
+    
     if None in (_addr, _ns, _queue):
         raise ValueError(f"missing required: addr={_addr}, ns={_ns}, queue={_queue}")
     queue = str(_queue)  
 
     # worker
-    client = await Client.connect(_addr, namespace=_ns)
+    client = await Client.connect(_addr, namespace=_ns,rpc_metadata=_rpc_metadata)
+
     worker = Worker(
         client,
         task_queue=queue,
